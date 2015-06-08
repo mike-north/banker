59c6652fa1427f384984963a19e2efa257311ca3
Mike North <northm@yahoo-inc.com>
2015-06-08 09:03:29 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const validateConfig = require('./src/utils/validate-config');
+const koa = require('koa');
+const app = koa();
+const Redis = require('ioredis');
+
+const configErrors = validateConfig.environment();
+
+if (configErrors.length > 0) {
+  process.stderr.write('\nConfiguration errors detected. Shutting down.\n');
+  process.exit();
+}
+
+const redis = new Redis({
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
+  password: process.env.REDIS_SECRET,
+});
+
+app.use(function* () {
+  this.body = 'hello world';
+});
+
+app.listen(process.env.PORT || 3000);
---
+++ src/utils/validate-config.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const chalk = require('chalk');
+
+const requiredEnvVars = ['REDIS_HOST', 'REDIS_PORT', 'REDIS_SECRET'];
+const secretEnvVars = ['REDIS_SECRET'];
+
+module.exports = {
+  environment() {
+    let errors = [];
+    requiredEnvVars.forEach(function(v) {
+      if (!process.env[v]) {
+        let errorStr = `Missing ENV variable: ${v}`;
+        errors.push(errorStr)
+        process.stderr.write(chalk.red(`${errorStr}\n`));
+      } else {
+        process.stdout.write(
+          chalk.green(
+            `${v}:\t${secretEnvVars.indexOf(v) >= 0 ?
+             '*********' :
+             process.env[v]}\n`
+          )
+        );
+      }
+    });
+    return errors;
+  },
+};
